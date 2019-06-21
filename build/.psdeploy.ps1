95806f3cf81718776698d98f98e295a91d49d03b
Francois-Xavier <fxcat@lazywinadmin.com>
2019-06-20 23:21:23 -0700

    Update psdeploy !deploy

--- build/.psdeploy.ps1
+++ build/.psdeploy.ps1
@@ -13,7 +13,7 @@ if(
 {
     Deploy -Name Module {
         By -DeploymentType PSGalleryModule {
-            FromSource -Source $env:modulePath\$env:moduleName\$env:moduleName.psd1
+            FromSource -Source (Join-Path -path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath "BuildOutput\$env:moduleName")
             To -Targets PSGallery
             WithOptions -Options @{
                 ApiKey = $env:PSGalleryKey
