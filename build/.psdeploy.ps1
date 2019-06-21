123b86da15d8eac4783b7b370058f563af9724cd
Francois-Xavier <fxcat@lazywinadmin.com>
2019-06-20 23:08:38 -0700

    Update deployment step

--- build/.psdeploy.ps1
+++ build/.psdeploy.ps1
@@ -1,9 +1,31 @@
-Deploy -Name Module {
-    By -DeploymentType PSGalleryModule {
-        FromSource -Source $env:modulePath\$env:moduleName\$env:moduleName.psd1
-        To -Targets PSGallery
-        WithOptions -Options @{
-            ApiKey = $env:PSGalleryKey
+<#
+    $env:PSGalleryKey is set in the Continous Integration plaform
+    $env:modulePath is set in the build.ps1
+    $env:moduleName is set in the build.ps1
+    $ENV:BH* are set by the BuildHelpers module
+#>
+if(
+    $env:modulePath -and
+    $env:BHBuildSystem -ne 'Unknown' -and
+    $env:BHBranchName -eq "master" -and
+    $env:BHCommitMessage -match '!deploy'
+)
+{
+    Deploy -Name Module {
+        By -DeploymentType PSGalleryModule {
+            FromSource -Source $env:modulePath\$env:moduleName\$env:moduleName.psd1
+            To -Targets PSGallery
+            WithOptions -Options @{
+                ApiKey = $env:PSGalleryKey
+            }
         }
     }
+}
+else
+{
+    "Skipping deployment: To deploy, ensure that...`n" +
+    "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
+    "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
+    "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)" |
+        Write-Host
 }
