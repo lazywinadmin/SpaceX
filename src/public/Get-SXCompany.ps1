dbb6ff2fc416abd9ae6d68919b993366a865109c
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-09 21:31:11 -0800

    Remove trailing spaces

--- src/public/Get-SXCompany.ps1
+++ src/public/Get-SXCompany.ps1
@@ -3,14 +3,14 @@ function Get-SXCompany
     <#
     .SYNOPSIS
     Retrieve SpaceX Company data
-    
+
     .DESCRIPTION
     Retrieve SpaceX Company data
-    
+
     .EXAMPLE
     C:\PS> Get-SXCompany
     Gets the company data
-    
+
     .NOTES
     https://github.com/lazywinadmin/SpaceX
     #>
