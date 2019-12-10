574152d20286f98faad63089464f58bc2721140b
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-09 21:31:21 -0800

    Remove trailing spaces

--- src/public/Get-SXDragon.ps1
+++ src/public/Get-SXDragon.ps1
@@ -2,13 +2,13 @@ function Get-SXDragon {
   <#
   .SYNOPSIS
   Retrieve SpaceX Dragon Capsule data
-  
+
   .DESCRIPTION
   Retrieve SpaceX Dragon Capsule data
 
   .PARAMETER ID
   Specify the Dragon Capsule ID
-  
+
   .EXAMPLE
   C:\PS> Get-SXDragon
   Gets data about all the dragons
@@ -16,7 +16,7 @@ function Get-SXDragon {
   .EXAMPLE
   Get-SXDragon -ID dragon1
   Gets data about the dragon with the specified ID
-  
+
   .NOTES
   https://github.com/lazywinadmin/SpaceX
   #>
