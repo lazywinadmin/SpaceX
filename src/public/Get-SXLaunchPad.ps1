26f81a64711f54fbfe4edbf570f577abc459adc8
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-09 21:31:33 -0800

    Remove trailing spaces

--- src/public/Get-SXLaunchPad.ps1
+++ src/public/Get-SXLaunchPad.ps1
@@ -2,13 +2,13 @@ function Get-SXLaunchpad {
     <#
     .SYNOPSIS
     Retrieve SpaceX launch sites data
-    
+
     .DESCRIPTION
     Retrieve SpaceX launch sites data
-    
+
     .PARAMETER LaunchPad
     The Site ID of a specific launch pad
-    
+
     .EXAMPLE
     C:\PS> Get-SXLaunchpad
     Gets data about all the launch pads
@@ -16,7 +16,7 @@ function Get-SXLaunchpad {
     .EXAMPLE
     C:\PS> Get-SXLaunchpad -Launchpad stls
     Gets data for the launch pad with the specified Site ID
-    
+
     .NOTES
     https://github.com/lazywinadmin/SpaceX
     #>
