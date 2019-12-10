d2da61f081198911f5a4020bccae4b6272895416
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-09 21:31:46 -0800

    Remove trailing spaces

--- src/public/Get-SXRocket.ps1
+++ src/public/Get-SXRocket.ps1
@@ -2,13 +2,13 @@ function Get-SXRocket {
     <#
     .SYNOPSIS
     Retrieve SpaceX rocket data
-    
+
     .DESCRIPTION
     Retrieve SpaceX rocket data
 
     .PARAMETER Rocket
     Specify the rocket name
-    
+
     .EXAMPLE
     Get-SXRocket
     Gets data about all the rockets
@@ -16,7 +16,7 @@ function Get-SXRocket {
     .EXAMPLE
     Get-SXRocket -Rocket falconheavy
     Gets data about the rocket with the specified Rocket ID
-    
+
     .NOTES
     https://github.com/lazywinadmin/SpaceX
     #>
