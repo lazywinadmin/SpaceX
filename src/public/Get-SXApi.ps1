c77d1e7f99f4fc43f88095596174020b6d365d2e
lazywinadmin <fxcat@lazywinadmin.com>
2019-12-09 21:31:02 -0800

    Remove trailing spaces

--- src/public/Get-SXApi.ps1
+++ src/public/Get-SXApi.ps1
@@ -3,14 +3,14 @@ function Get-SXApi
     <#
     .SYNOPSIS
     Retrieve SpaceX API data
-    
+
     .DESCRIPTION
     Retrieve SpaceX API data
-    
+
     .EXAMPLE
     C:\PS> Get-SXApi
     Gets the API data
-    
+
     .NOTES
     https://github.com/lazywinadmin/SpaceX
     #>
